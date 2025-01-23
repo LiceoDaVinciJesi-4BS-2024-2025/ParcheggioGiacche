810dc9ec83ad64ecb0b7675bc6cb245e4b253a49
Mattia Giacchè <s11082922@liceodavincijesi.edu.it>
2025-01-23 09:00:18 +0100

    ade il parcheggio si libera da solo

--- parcheggio/esercizio.py
+++ parcheggio/esercizio.py
@@ -1,5 +1,6 @@
 from posto_mezzo import *
 
+
 class Parcheggio:
     def __init__(self):
         #-----------------------------------------------------------------
@@ -18,9 +19,20 @@ class Parcheggio:
         self.__listaVeicoliParcheggiati = lista[0].split(sep=';')
         while '' in self.__listaVeicoliParcheggiati:
             self.__listaVeicoliParcheggiati.remove('')
+        for x in self.__listaVeicoliParcheggiati:
+            listaParametriMezzo = x.split(sep='_')
+            if listaParametriMezzo[1] > str(datetime.datetime.today()):
+                if 'Auto' in listaParametriMezzo[0]:
+                    self.__postiAutoLiberi += 1
+                elif 'Moto' in listaParametriMezzo[0]:
+                    self.__postiMotoLiberi += 1
+                self.__listaVeicoliParcheggiati.remove(x)
+        lista.remove(lista[0])
+        self.__guadagno = lista[0]
+        if self.__guadagno == '':
+            self.__guadagno = 0
+            
         #-----------------------------------------------------------------
-        self.__guadagno = 0
-
     
     def __str__(self):
         return str(self.__dict__)
@@ -45,23 +57,24 @@ class Parcheggio:
             self.__postiAutoLiberi -= 1
             orePermanenza = posto.dataOraFine - datetime.datetime.today()
             saldoDaPagare = (int(orePermanenza.total_seconds())/60) * 1.5
-            self.__listaVeicoliParcheggiati.append(posto.occupante)
+            self.__listaVeicoliParcheggiati.append(f'{posto.occupante}_{posto.dataOraFine}')
         
         elif type(posto.occupante) == Moto and self.__postiMotoLiberi != 0:
             self.__postiMotoLiberi -= 1
             orePermanenza = posto.dataOraFine - datetime.datetime.today()
             saldoDaPagare = (int(orePermanenza.total_seconds())/60) * 1.2
-            self.__listaVeicoliParcheggiati.append(posto.occupante)
+            self.__listaVeicoliParcheggiati.append(f'{posto.occupante}_{posto.dataOraFine}')
 
         else:
             raise ValueError('Non abbiamo posti liberi per quel veicolo')
         
-        self.__guadagno += saldoDaPagare
+        self.__guadagno = float(self.__guadagno) + saldoDaPagare
         #-----------------------------------------------------------------
         file = open('parkdata.txt' , 'w')
         file.write(f'{self.__postiAutoLiberi}\n{self.__postiMotoLiberi}\n')
         for x in self.__listaVeicoliParcheggiati:
             file.write(f'{x};')
+        file.write(f'\n{self.__guadagno}')
         file.close()
         #-----------------------------------------------------------------
         return f'Si devono pagare {saldoDaPagare} euro'
